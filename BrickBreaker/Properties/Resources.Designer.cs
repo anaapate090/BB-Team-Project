cd5ac9906fd5bf7168c0f263eeaf9f1a67772854
Adam Wingert <adamwing415@ed.amdsb.ca>
2020-10-21 10:22:49 -0400

    added title image

--- BrickBreaker/Properties/Resources.Designer.cs
+++ BrickBreaker/Properties/Resources.Designer.cs
@@ -89,5 +89,15 @@ namespace BrickBreaker.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap title_screen_brick_breaker {
+            get {
+                object obj = ResourceManager.GetObject("title_screen_brick_breaker", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
