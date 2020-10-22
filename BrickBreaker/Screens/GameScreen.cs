131cfdb0251b20845ca8771daa14dcc0558ae4a5
Adam Wingert <adamwing415@ed.amdsb.ca>
2020-10-22 11:01:07 -0400

    changed score value

--- BrickBreaker/Screens/GameScreen.cs
+++ BrickBreaker/Screens/GameScreen.cs
@@ -364,6 +364,9 @@ namespace BrickBreaker
                 // Moves the ball back to origin
                 ball.x = ((paddle.x - (ball.size / 2)) + (paddle.width / 2));
                 ball.y = (this.Height - paddle.height) - 85;
+
+                paddle.x = this.Width / 2 - paddle.width / 2;
+
                 #endregion
 
 
@@ -406,7 +409,7 @@ namespace BrickBreaker
                     }
 
                     b.hp--;
-                    score++;
+                    score += 100;
                   
 
                     if (b.hp == 0)
